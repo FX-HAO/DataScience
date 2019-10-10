a55509beab8eb733e3e9214c8b23e808e7cc6400
Fuxin Hao <haofxpro@gmail.com>
2019-10-10 11:21:42 +0800

    fizzbuzz

--- neural_network/fizzbuzz/main.py
+++ neural_network/fizzbuzz/main.py
@@ -7,32 +7,42 @@ from keras.layers import Dense
 from sklearn.model_selection import train_test_split
 from sklearn import metrics
 
-# def fizz_buzz_encode(i):
-#     if   i % 15 == 0: return np.array([0, 0, 0, 1])
-#     elif i % 5  == 0: return np.array([0, 0, 1, 0])
-#     elif i % 3  == 0: return np.array([0, 1, 0, 0])
-#     else:             return np.array([1, 0, 0, 0])
 def fizz_buzz_encode(i):
-    if i % 15 == 0: return 1
-    if i % 5 == 0: return 2
-    if i % 3 == 0: return 3
-    else: return 4
+    if   i % 15 == 0: return np.array([0, 0, 0, 1])
+    elif i % 5  == 0: return np.array([0, 0, 1, 0])
+    elif i % 3  == 0: return np.array([0, 1, 0, 0])
+    else:             return np.array([1, 0, 0, 0])
 
 def binary_encode(i, num_digits):
     return np.array([i >> d & 1 for d in range(num_digits)])
 
 NUM_DIGITS = 16
 
-X = np.array([binary_encode(i, NUM_DIGITS) for i in range(2 ** NUM_DIGITS)])
-Y = np.array([fizz_buzz_encode(i) for i in range(2 ** NUM_DIGITS)])
+X = np.array([binary_encode(i, NUM_DIGITS) for i in range(101, 2 ** NUM_DIGITS)])
+Y = np.array([fizz_buzz_encode(i) for i in range(101, 2 ** NUM_DIGITS)])
 X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.25, random_state=42)
 
 model = Sequential()
 model.add(Dense(64, input_dim=16, activation='relu'))
 model.add(Dense(64, activation='relu'))
-model.add(Dense(1))
-model.compile(optimizer='rmsprop', loss='mse', metrics=['mae'])
+model.add(Dense(4, activation='softmax'))
+model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
 model.fit(X_train, y_train, epochs=10, verbose=1)
-y_pred = model.predict(X_test).astype('int')
-print(metrics.confusion_matrix(y_test, y_pred))
-print("Accuracy:", metrics.accuracy_score(y_test, y_pred))
+y_pred = model.predict_classes(X_test)
+score = model.evaluate(X_test, y_test, batch_size=2000)
+print(score)
+
+X = np.array([binary_encode(i, NUM_DIGITS) for i in range(1, 101)])
+y = model.predict_classes(X)
+result = []
+for i in range(len(y)):
+    if y[i] == 3:
+        result.append('fizzbuzz')
+    elif y[i] == 2:
+        result.append('buzz')
+    elif y[i] == 1:
+        result.append('fizz')
+    elif y[i] == 0:
+        result.append(str(i+1))
+
+print(result)
